c4d2e2379fd1da40f06566384c192ef99d837830
Petermusau <petermusau13452@gmail.com>
2019-05-09 08:17:36 +0300

    update

--- girl/views.py
+++ girl/views.py
@@ -7,6 +7,12 @@ class MemberListView(ListView):
     model =Member
     template_name ='girl/index.html'
     context_object_name = 'profiles'
+    ordering = ['name']
+    def get_queryset(self):
+        query=self.request.GET.get('g')
+        if query:
+            return Member.object.filter(name_icontains=query)| Member.object.filter(occupations__icontains=query)
+        else:return Member.object.all()
 
 class MemberCreateView(CreateView):
     model = Member
